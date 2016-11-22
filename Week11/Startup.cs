ed07483065de41dcbc9b7362df3254cac9939707
ifotn <rich@infrontofthenet.com>
2016-11-22 09:08:08 -0500

    Add project files.

---
+++ Week11/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week11.Startup))]
+namespace Week11
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
